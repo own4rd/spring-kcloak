cb5f94028113ce2c96f5323aa0b761a931ca7b8d
Wagner Palmeira <wagnerpalmeira.ti@gmail.com>
2024-11-28 23:08:18 -0300

    Önly Sample

---
+++ src/test/java/br/com/lowlevel/spring_keycloak/SpringKeycloakApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lowlevel.spring_keycloak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKeycloakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
